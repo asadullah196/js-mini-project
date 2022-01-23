32a283ded73142c5fd20dfd268263aee049d7447
Asadullah Galib <md.asadullah196@gmail.com>
2022-01-23 19:33:23 +0600

    Upper Case Buttion added

--- 01 case-change-js/js/main.js
+++ 01 case-change-js/js/main.js
@@ -13,3 +13,19 @@ function lowerCase() {
     }
     document.getElementById('userInput').value = text.toLowerCase();
 }
+
+/**
+ * upperCase Converstion
+ * 
+ * @returns string/input
+ */
+
+ function upperCase() {
+    var x = document.getElementById("formInput");
+    var text = "";
+    var i;
+    for (i = 0; i < x.length; i++) {
+        text += x.elements[i].value;
+    }
+    document.getElementById('userInput').value = text.toUpperCase();
+}
