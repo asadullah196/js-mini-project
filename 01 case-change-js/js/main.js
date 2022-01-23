53dc3e2998b66eb91a76bd026e1dcc1427cb3473
Asadullah Galib <md.asadullah196@gmail.com>
2022-01-23 19:39:47 +0600

    Basic task has done. Now code optimization and features need to update

--- 01 case-change-js/js/main.js
+++ 01 case-change-js/js/main.js
@@ -29,3 +29,19 @@ function lowerCase() {
     }
     document.getElementById('userInput').value = text.toUpperCase();
 }
+
+/**
+ * sentanceCase Converstion
+ * 
+ * @returns string/input
+ */
+
+ function sentanceCase() {
+    var x = document.getElementById("formInput");
+    var text = "";
+    var i;
+    for (i = 0; i < x.length; i++) {
+        text += x.elements[i].value;
+    }
+    document.getElementById('userInput').value = text.toUpperCase();
+}
