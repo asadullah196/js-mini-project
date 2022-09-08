711db9b744e1067825fe599c5da24e005f229e1c
Asadullah Galib <md.asadullah196@gmail.com>
2022-09-08 21:12:32 +0600

    Click counter added and it's working

--- 02 use-of-local-storage/js/main.js
+++ 02 use-of-local-storage/js/main.js
@@ -3,3 +3,15 @@
  */
 
 console.log("Asadullah Galib!");
+
+// Counts the click numbers
+
+var clicks = 0;
+var buttonImage = document.getElementById('buttonImage');
+buttonImage.onclick = function () {
+    console.log(++clicks);
+}
+
+function changeTheme() {
+    console.log("Hello");
+}
