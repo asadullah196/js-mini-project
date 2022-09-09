3e11f28770bc8000ad6ef1552feb3ecc1f5a6f4e
Asadullah Galib <md.asadullah196@gmail.com>
2022-09-09 21:01:10 +0600

    Primary task done

--- 02 use-of-local-storage/js/main.js
+++ 02 use-of-local-storage/js/main.js
@@ -11,11 +11,15 @@ var buttonImage = document.getElementById('buttonImage');
 buttonImage.onclick = function () {
     
     if(clicks%2 === 0) {
-        console.log("Even Number " + clicks);
+        //console.log("Even Number " + clicks);
         document.getElementById('buttonImage').src='./img/sun.png';
+        document.getElementById("myName").style.color = '#000000';
+        document.getElementById("main-body").style.backgroundColor = '#ffffff';
     }else {
-        console.log("Odd Number " + clicks);
+        //console.log("Odd Number " + clicks);
         document.getElementById('buttonImage').src='./img/moon.png';
+        document.getElementById("myName").style.color = '#ffffff';
+        document.getElementById("main-body").style.backgroundColor = '#000000';
     }
 
     clicks++;
