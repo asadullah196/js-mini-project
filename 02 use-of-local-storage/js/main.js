5930184743d11967c96c031d29618a1e5fe8654f
Asadullah Galib <md.asadullah196@gmail.com>
2022-09-11 23:25:51 +0600

    Not fix yet but hoping to fix it soon.

--- 02 use-of-local-storage/js/main.js
+++ 02 use-of-local-storage/js/main.js
@@ -1,95 +1,56 @@
-// /**
-//  * Just "Hello" statement for test
-//  */
+/**
+ * Just "Hello" statement for test
+ */
 
-// //console.log("Asadullah Galib!");
-
-
-// // Counts the click numbers
-
-// var clicks = 0;
-// var buttonImage1 = document.getElementById('buttonImage');
+//console.log("Asadullah Galib!");
 
 
-// buttonImage1.onclick = function () {
+// Counts the click numbers
 
-//     //localStorage.setItem("myNumber", "1");
-
-//     //let myNum = localStorage.getItem("myNumber");
-//     // console.log(myNum);
+var clicks = 0;
+var buttonImage1 = document.getElementById('buttonImage');
 
 
-//     if (clicks % 2 === 0) {
-//         //console.log("Even Number " + clicks);
+buttonImage1.onclick = function () {
 
-//         // Pass Data to local storage
+    if (clicks % 2 === 0) {
+        //console.log("Even Number " + clicks);
 
-//         localStorage.setItem("imageName", "./img/sun.png");
-//         //console.log(localStorage.getItem("imageName"));
+        // Pass Data to local storage
 
-//         document.getElementById('buttonImage').src = './img/moon.png';
+        localStorage.setItem("imageName", "./img/sun.png");
+        //console.log(localStorage.getItem("imageName"));
 
-//         //document.getElementById('buttonImage').src = localStorage.getItem("imageName");
+        if (localStorage) {
+            //console.log(localStorage.getItem("imageName"));
+            document.getElementById('buttonImage').src = localStorage.getItem("imageName");
 
-//         //console.log("Test 3 " + document.getElementById('buttonImage').src);
+        } else {
+            document.getElementById('buttonImage').src = './img/sun.png';
+        }
 
-//         /*
-//         if (localStorage) {
-//             //console.log(localStorage.getItem("imageName"));
+        document.getElementById("myName").style.color = '#000000';
+        document.getElementById("main-body").style.backgroundColor = '#ffffff';
+    } else {
 
-//             console.log("Test 1 " + localStorage.getItem("imageName"));
+        // Pass Data to local storage
 
-//             document.getElementById('buttonImage').src = localStorage.getItem("imageName");
+        localStorage.setItem("imageName", "./img/moon.png");
 
-//             console.log("Test 2 " + document.getElementById('buttonImage').src);
+        if (localStorage) {
+            //console.log(localStorage.getItem("imageName"));
+            document.getElementById('buttonImage').src = localStorage.getItem("imageName");
 
-//         } else {
-//             document.getElementById('buttonImage').src = './img/sun.png';
-//         }
+        } else {
+            document.getElementById('buttonImage').src = './img/moon.png';
+        }
 
+        document.getElementById("myName").style.color = '#ffffff';
+        document.getElementById("main-body").style.backgroundColor = '#000000';
+    }
 
-//         */
+    clicks++;
 
-//         //document.getElementById('buttonImage').src = localStorage.getItem("imageName");
+    console.log(localStorage.getItem("imageName"));
 
-//         document.getElementById("myName").style.color = '#000000';
-//         document.getElementById("main-body").style.backgroundColor = '#ffffff';
-//     } else {
-
-//         // Pass Data to local storage
-
-//         localStorage.setItem("imageName", "./img/moon.png");
-
-//         document.getElementById('buttonImage').src = './img/moon.png';
-
-//         //document.getElementById('buttonImage').src = localStorage.getItem("imageName");
-
-//         //console.log("Test 3 " + document.getElementById('buttonImage').src);
-
-//         /*
-//         if (localStorage) {
-//             //console.log(localStorage.getItem("imageName"));
-//             document.getElementById('buttonImage').src = localStorage.getItem("imageName");
-
-//             console.log("Test 3 " + document.getElementById('buttonImage').src);
-
-//         } else {
-//             document.getElementById('buttonImage').src = './img/moon.png';
-//         }
-
-//         */
-
-//         //console.log(localStorage.getItem("imageName"));
-//         //document.getElementById('buttonImage').src = localStorage.getItem("imageName");
-
-//         //console.log("Odd Number " + clicks);
-//         //document.getElementById('buttonImage').src='./img/moon.png';
-//         document.getElementById("myName").style.color = '#ffffff';
-//         document.getElementById("main-body").style.backgroundColor = '#000000';
-//     }
-
-//     clicks++;
-
-//     console.log(localStorage.getItem("imageName"));
-
-// }
+}
