808cf9c6fcb28b636bc1c75afac1c5f8215266c4
Asadullah Galib <md.asadullah196@gmail.com>
2022-09-11 22:37:37 +0600

    Storage still not working

--- 02 use-of-local-storage/js/main.js
+++ 02 use-of-local-storage/js/main.js
@@ -4,84 +4,92 @@
 
 //console.log("Asadullah Galib!");
 
-/*
-clickCounter();
-
-function clickCounter() {
-    if (localStorage.clickcount) {
-
-        console.log(localStorage.clickcount);
-
-        localStorage.clickcount = Number(localStorage.clickcount) + 1;
-    } else {
-        localStorage.clickcount = 1;
-    }
-    document.getElementById("demo").innerHTML = localStorage.clickcount;
-}
-
-*/
 
 // Counts the click numbers
 
 var clicks = 0;
-var buttonImage = document.getElementById('buttonImage');
-
-function changeTheme() {
-    buttonImage.onclick = function () {
-
-        //localStorage.setItem("myNumber", "1");
-
-        //let myNum = localStorage.getItem("myNumber");
-        // console.log(myNum);
+var buttonImage1 = document.getElementById('buttonImage');
 
 
-        if (clicks % 2 === 0) {
-            //console.log("Even Number " + clicks);
+buttonImage1.onclick = function () {
 
-            // Pass Data to local storage
+    //localStorage.setItem("myNumber", "1");
 
-            localStorage.setItem("imageName", "./img/sun.png");
+    //let myNum = localStorage.getItem("myNumber");
+    // console.log(myNum);
+
+
+    if (clicks % 2 === 0) {
+        //console.log("Even Number " + clicks);
+
+        // Pass Data to local storage
+
+        localStorage.setItem("imageName", "./img/sun.png");
+        //console.log(localStorage.getItem("imageName"));
+
+        document.getElementById('buttonImage').src = './img/moon.png';
+
+        //document.getElementById('buttonImage').src = localStorage.getItem("imageName");
+
+        //console.log("Test 3 " + document.getElementById('buttonImage').src);
+
+        /*
+        if (localStorage) {
             //console.log(localStorage.getItem("imageName"));
 
-            if (localStorage) {
-                //console.log(localStorage.getItem("imageName"));
-                document.getElementById('buttonImage').src = localStorage.getItem("imageName");
-            } else {
-                document.getElementById('buttonImage').src = './img/sun.png';
-            }
+            console.log("Test 1 " + localStorage.getItem("imageName"));
 
+            document.getElementById('buttonImage').src = localStorage.getItem("imageName");
 
-            //document.getElementById('buttonImage').src = localStorage.getItem("imageName");
+            console.log("Test 2 " + document.getElementById('buttonImage').src);
 
-            document.getElementById("myName").style.color = '#000000';
-            document.getElementById("main-body").style.backgroundColor = '#ffffff';
         } else {
-
-            // Pass Data to local storage
-
-            localStorage.setItem("imageName", "./img/moon.png");
-
-            if (localStorage) {
-                //console.log(localStorage.getItem("imageName"));
-                document.getElementById('buttonImage').src = localStorage.getItem("imageName");
-            } else {
-                document.getElementById('buttonImage').src = './img/moon.png';
-            }
-
-            //console.log(localStorage.getItem("imageName"));
-            //document.getElementById('buttonImage').src = localStorage.getItem("imageName");
-
-            //console.log("Odd Number " + clicks);
-            //document.getElementById('buttonImage').src='./img/moon.png';
-            document.getElementById("myName").style.color = '#ffffff';
-            document.getElementById("main-body").style.backgroundColor = '#000000';
+            document.getElementById('buttonImage').src = './img/sun.png';
         }
 
-        clicks++;
 
-        console.log(localStorage.getItem("imageName"));
+        */
 
+        //document.getElementById('buttonImage').src = localStorage.getItem("imageName");
+
+        document.getElementById("myName").style.color = '#000000';
+        document.getElementById("main-body").style.backgroundColor = '#ffffff';
+    } else {
+
+        // Pass Data to local storage
+
+        localStorage.setItem("imageName", "./img/moon.png");
+
+        document.getElementById('buttonImage').src = './img/moon.png';
+
+        //document.getElementById('buttonImage').src = localStorage.getItem("imageName");
+
+        //console.log("Test 3 " + document.getElementById('buttonImage').src);
+
+        /*
+        if (localStorage) {
+            //console.log(localStorage.getItem("imageName"));
+            document.getElementById('buttonImage').src = localStorage.getItem("imageName");
+
+            console.log("Test 3 " + document.getElementById('buttonImage').src);
+
+        } else {
+            document.getElementById('buttonImage').src = './img/moon.png';
+        }
+
+        */
+
+        //console.log(localStorage.getItem("imageName"));
+        //document.getElementById('buttonImage').src = localStorage.getItem("imageName");
+
+        //console.log("Odd Number " + clicks);
+        //document.getElementById('buttonImage').src='./img/moon.png';
+        document.getElementById("myName").style.color = '#ffffff';
+        document.getElementById("main-body").style.backgroundColor = '#000000';
     }
 
-    console.log("Hello");
+    clicks++;
+
+    console.log(localStorage.getItem("imageName"));
+
 }
