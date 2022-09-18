1c5cb6f79af710a75157884e136b2c22dccc05e5
Asadullah Galib <md.asadullah196@gmail.com>
2022-09-18 21:32:33 +0600

    Data collected from inout box

--- 03 to-do application/js/main.js
+++ 03 to-do application/js/main.js
@@ -5,3 +5,9 @@ let myName = "Asadullah";
 console.log("myName");
 
 let myId = document.getElementById("todo-submit").innerHTML;
+
+function saveTodo() {
+    console.log(document.getElementById("todo-input").value);
+
+    console.log("Asadullah Console!");
+}
