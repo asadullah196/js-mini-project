23cfcb93dc2d11ae2707fafed4fd594d5f017dc3
Asadullah Galib <md.asadullah196@gmail.com>
2022-09-18 22:59:30 +0600

    Code updated

--- 03 to-do application/js/main.js
+++ 03 to-do application/js/main.js
@@ -7,10 +7,12 @@ let myId = document.getElementById("todo-submit").innerHTML;
 function saveTodo() {
     const todoInput = document.getElementById("todo-input").value;
 
+    /* code for testing
     let myNumber = 2;
 
     if (myNumber === 2) {
         alert("Data Saved!");
         document.getElementById("todo-input").value = '';
     }
+    */
 }
