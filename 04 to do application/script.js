49b6105f16a5c5265047171ef97cc4cc3281d90b
Asadullah Galib <md.asadullah196@gmail.com>
2022-10-09 17:58:57 +0600

    js headed added

---
+++ 04 to do application/script.js
@@ -0,0 +1,8 @@
+/*
+ * Title: To Do Application using vanilla JS DOM
+ * Description: This JS file has all the JS functions necessary to control the to do application
+ * Author: Asadullah Galib ( Orginal Sumit vai)
+ * Date: 09/10/2022
+ *
+ */
+
