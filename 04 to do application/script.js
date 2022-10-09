e841a355a26e986eab3c75d1a30d5044b3e0483a
Asadullah Galib <md.asadullah196@gmail.com>
2022-10-09 18:11:24 +0600

    list items created and checked

--- 04 to do application/script.js
+++ 04 to do application/script.js
@@ -11,3 +11,20 @@ let newTask = document.querySelector('#new-task');
 let form = document.querySelector('form');
 let todoUIs = document.querySelector('#items');
 let finishedTask = document.querySelector('.complete-list');
+
+// Function for creating new task
+let creatTask = function(task) {
+    let listItem = document.createElement('li');
+    let checkBox = document.createElement('input');
+    let label = document.createElement('label');
+
+    label.innerHTML = task;
+    checkBox.type = 'checkbox';
+
+    listItem.appendChild(checkBox);
+    listItem.appendChild(label);
+
+    // console.log(listItem);
+    
+    return listItem;
+}
