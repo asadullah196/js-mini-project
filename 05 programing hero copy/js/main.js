44ac3c0abc177cb0a2ff4f0a81d178e61f44b21c
Asadullah Galib <md.asadullah196@gmail.com>
2022-10-10 20:33:29 +0600

    unknown changes

--- 05 programing hero copy/js/main.js
+++ 05 programing hero copy/js/main.js
@@ -1,9 +1,26 @@
 const milestonesData = JSON.parse(data).data;
 
 // Load couse milestone data
-function loadMilestones(){
+function loadMilestones() {
     const milestone = document.querySelector('.milestones');
-    milestone.innerHTML = milestonesData.join(" ");
+    milestone.innerHTML = `${milestonesData.map(function (milestone) {
+        return `<div class="milestone border-b">
+        <div class="flex">
+          <div class="checkbox"><input type="checkbox" /></div>
+          <div>
+            <p>
+              Milestone 1 name
+              <span><i class="fas fa-chevron-down"></i></span>
+            </p>
+          </div>
+        </div>
+        <div class="hidden_panel">
+          <div class="module border-b">
+            <p>Module Name</p>
+          </div>
+        </div>
+      </div>`
+    }).join(" ")}`;
 }
 
 // Call the milistone function
